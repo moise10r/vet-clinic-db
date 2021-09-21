050a7bbd4647153ea5a774f3413042f2c4fb8693
moise10r <moiserushanika2006@gmail.com>
2021-09-21 10:53:03 +0200

    insert the third animal

--- data.sql
+++ data.sql
@@ -3,3 +3,4 @@
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon', '02-03-2020', 0, TRUE, 10.23);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon', '11-15-2020', 2, TRUE, 8));
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Pikachu', '01-7-2021', 1, FALSE, 15.04);
