50acbab45021eb6191ccc18795750261bcc96ba4
moise10r <moiserushanika2006@gmail.com>
2021-09-21 10:52:21 +0200

    insert the second animal

--- data.sql
+++ data.sql
@@ -1,4 +1,5 @@
 /* Populate database with sample data. */
 
 
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Agumon', '02-03-2020', 0, TRUE, 10.23);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon', '02-03-2020', 0, TRUE, 10.23);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon', '11-15-2020', 2, TRUE, 8));
