185875188b134584d684c5d9f8c720c9bd37eb38
moise10r <moiserushanika2006@gmail.com>
2021-09-21 10:53:31 +0200

    insert the fourth animal

--- data.sql
+++ data.sql
@@ -4,3 +4,4 @@
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon', '02-03-2020', 0, TRUE, 10.23);
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon', '11-15-2020', 2, TRUE, 8));
 INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Pikachu', '01-7-2021', 1, FALSE, 15.04);
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Devimon', '05-12-2017', 5, TRUE, 11);
