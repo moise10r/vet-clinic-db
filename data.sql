ed2c2c6c1b5eadbfdbf52066e8fa5c53affdcf50
moise10r <moiserushanika2006@gmail.com>
2021-09-21 10:51:05 +0200

    insert first animal

--- data.sql
+++ data.sql
@@ -1,5 +1,4 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES('Agumon', '02-03-2020', 0, TRUE, 10.23);
