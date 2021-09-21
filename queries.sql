53c48cd8e07e8500122a6561b77f66c24b4baf25
moise10r <moiserushanika2006@gmail.com>
2021-09-21 11:19:23 +0200

    Find all animals not named Gabumon.

--- queries.sql
+++ queries.sql
@@ -18,3 +18,7 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg>10.5;
 
 -- Find all animals that are neutered.
 SELECT * FROM animals WHERE neutered=true;
+
+-- Find all animals not named Gabumon.
+SELECT * FROM animals WHERE NOT name='Gabumon';
+
