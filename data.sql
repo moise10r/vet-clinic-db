ccd8efccb39e0f33a9b9f4101d1f029d6c8a3ad5
moise10r <moiserushanika2006@gmail.com>
2021-09-25 00:59:18 +0200

    Refractor vets insertion file

--- data.sql
+++ data.sql
@@ -179,78 +179,70 @@ END;
 
 -- Insert data into the 'specializations' table
 
-BEGIN;
-INSERT INTO specializations (vet_id, species_id) 
-  VALUES ((SELECT id FROM vets WHERE name='William Tatcher'),
-     (SELECT id FROM species WHERE name='Pokemon')),
-    ((SELECT id FROM vets WHERE name='Stephanie Mendez'),
-     (SELECT id FROM species WHERE name='Digimon')),
-    ((SELECT id FROM vets WHERE name='Stephanie Mendez'),
-     (SELECT id FROM species WHERE name='Pokemon')),
-    ((SELECT id FROM vets WHERE name='Jack Harkness'),
-     (SELECT id FROM species WHERE name='Digimon'));    
-END;
+/* Query multiple tables */
 
-BEGIN;
-INSERT INTO visits (vet_id, animal_id, visited) 
-                    VALUES ((SELECT id FROM vets WHERE name='William Tatcher'),
-    (SELECT id FROM animals WHERE name='Agumon'),
-    '2020-05-24'),
-    ((SELECT id FROM vets WHERE name='Stephanie Mendez'),
-    (SELECT id FROM animals WHERE name='Agumon'),
-    '2020-04-22'),
-    ((SELECT id FROM vets WHERE name='Jack Harkness'),
-    (SELECT id FROM animals WHERE name='Gabumon'),
-    '2021-02-02'),
-    ((SELECT id FROM vets WHERE name='Maisy Smith'),
-    (SELECT id FROM animals WHERE name='Pikachu'),
-    '2020-01-05'),
-    ((SELECT id FROM vets WHERE name='Maisy Smith'),
-    (SELECT id FROM animals WHERE name='Pikachu'),
-    '2020-03-08'),
-    ((SELECT id FROM vets WHERE name='Maisy Smith'),
-    (SELECT id FROM animals WHERE name='Pikachu'),
-    '2020-05-14'),
-    ((SELECT id FROM vets WHERE name='Stephanie Mendez'),
-    (SELECT id FROM animals WHERE name='Devimon'),
-    '2021-05-04'),
-    ((SELECT id FROM vets WHERE name='Jack Harkness'),
-    (SELECT id FROM animals WHERE name='Charmander'),
-    '2021-02-24'),
-    ((SELECT id FROM vets WHERE name='Maisy Smith'),
-    (SELECT id FROM animals WHERE name='Plantmon'),
-    '2019-12-21'),
-    ((SELECT id FROM vets WHERE name='William Tatcher'),
-    (SELECT id FROM animals WHERE name='Plantmon'),
-    '2020-08-10'),
-    ((SELECT id FROM vets WHERE name='Maisy Smith'),
-    (SELECT id FROM animals WHERE name='Plantmon'),
-    '2021-04-07'),
-    ((SELECT id FROM vets WHERE name='Stephanie Mendez'),
-    (SELECT id FROM animals WHERE name='Squirtle'),
-    '2019-09-29'),
-    ((SELECT id FROM vets WHERE name='Jack Harkness'),
-    (SELECT id FROM animals WHERE name='Angemon'),
-    '2020-10-03'),
-    ((SELECT id FROM vets WHERE name='Jack Harkness'),
-    (SELECT id FROM animals WHERE name='Angemon'),
-    '2020-11-04'),
-    ((SELECT id FROM vets WHERE name='Maisy Smith'),
-    (SELECT id FROM animals WHERE name='Boarmon'),
-    '2019-01-24'),
-    ((SELECT id FROM vets WHERE name='Maisy Smith'),
-    (SELECT id FROM animals WHERE name='Boarmon'),
-    '2019-05-15'),
-    ((SELECT id FROM vets WHERE name='Maisy Smith'),
-    (SELECT id FROM animals WHERE name='Boarmon'),
-    '2020-02-27'),
-    ((SELECT id FROM vets WHERE name='Maisy Smith'),
-    (SELECT id FROM animals WHERE name='Boarmon'),
-    '-2020-08-03'),
-    ((SELECT id FROM vets WHERE name='Stephanie Mendez'),
-    (SELECT id FROM animals WHERE name='Blossom'),
-    '2020-05-24'),
-    ((SELECT id FROM vets WHERE name='William Tatcher'),
-    (SELECT id FROM animals WHERE name='Blossom'),
-    '2021-01-11');
-END;    
+BEGIN TRANSACTION;
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34);
+INSERT INTO owners (full_name, age) VALUES ('Jennifer Orwell', 19);
+INSERT INTO owners (full_name, age) VALUES ('Bob', 45);
+INSERT INTO owners (full_name, age) VALUES ('Melody Pond', 77);
+INSERT INTO owners (full_name, age) VALUES ('Dean Winchester', 14);
+INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
+COMMIT TRANSACTION;
+
+BEGIN TRANSACTION;
+INSERT INTO species (name) VALUES ('Pokemon');
+INSERT INTO species (name) VALUES ('Digimon');
+COMMIT TRANSACTION;
+
+BEGIN TRANSACTION;
+UPDATE animals SET species_id = 1 WHERE name LIKE '%mon';
+UPDATE animals SET species_id = 2 WHERE species_id IS NULL;
+COMMIT TRANSACTION;
+
+BEGIN TRANSACTION;
+UPDATE animals SET owner_id = 1 WHERE name LIKE 'Agumon';
+UPDATE animals SET owner_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
+UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
+COMMIT TRANSACTION;
+
+/* Add "join table" for visits */
+
+BEGIN TRANSACTION;
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('William Tatcher', 45, 'Apr 23, 2000');
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Maisy Smith', 26, 'Jan 17, 2019');
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Stephanie Mendez', 64, 'May 4, 1981');
+INSERT INTO vets (name, age, date_of_graduation) VALUES ('Jack Harkness', 38, 'Jun 8, 2008');
+COMMIT TRANSACTION;
+
+-- BEGIN TRANSACTION;
+-- INSERT INTO specialization (vets_id, species_id) VALUES (1, 1);
+-- INSERT INTO specialization (vets_id, species_id) VALUES (3, 2);
+-- INSERT INTO specialization (vets_id, species_id) VALUES (3, 1);
+-- INSERT INTO specialization (vets_id, species_id) VALUES (4, 2);
+-- COMMIT TRANSACTION;
+
+-- BEGIN TRANSACTION;
+-- INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (1, 1, 'May 24, 2020');
+-- INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (1, 3, 'Jul 22, 2020');
+-- INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (2, 4, 'Feb 2, 2021');
+-- INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (3, 2, 'Jan 5, 2020');
+-- INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (3, 2, 'Mar 8, 2020');
+-- INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (3, 2, 'May 14, 2020');
+-- INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (4, 3, 'May 4, 2021');
+-- INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (5, 4, 'Feb 24, 2021');
+-- INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (6, 2, 'Dec 21, 2019');
+-- INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (6, 1, 'Aug 10, 2020');
+-- INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (6, 2, 'Apr 7, 2021');
+-- INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (7, 3, 'Sep 29, 2019');
+-- INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (8, 4, 'Oct 3, 2020');
+-- INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (8, 4, 'Nov 4, 2020');
+-- INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (9, 2, 'Jan 24, 2019');
+-- INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (9, 2, 'May 15, 2019');
+-- INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (9, 2, 'Feb 27, 2020');
+-- INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (9, 2, 'Aug 3, 2020');
+-- INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (10, 3, 'May 24, 2020');
+-- INSERT INTO visits (animals_id, vets_id, date_of_visit) VALUES (10, 1, 'Jan 11, 2021');
+-- COMMIT TRANSACTION;
