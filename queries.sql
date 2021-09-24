5318034592432eb4780b7d6d01615880b33e3858
moise10r <moiserushanika2006@gmail.com>
2021-09-24 23:58:14 +0200

    Select animals name and Count visits as first_visits

--- queries.sql
+++ queries.sql
@@ -103,12 +103,12 @@ SELECT animals.name, COUNT(visits.animals_id) AS visit_count FROM visits
     GROUP BY animals.name, visits.animals_id
     ORDER BY visit_count DESC LIMIT 1;
 
--- SELECT animals.name, visits.date_of_visit AS first_visit FROM visits
---     INNER JOIN vets ON vets.id = visits.vets_id
---     INNER JOIN animals ON animals.id = visits.animals_id
---     WHERE vets.name = 'Maisy Smith'
---     GROUP BY animals.name, visits.date_of_visit
---     ORDER BY first_visit LIMIT 1;
+SELECT animals.name, visits.date_of_visit AS first_visit FROM visits
+    INNER JOIN vets ON vets.id = visits.vets_id
+    INNER JOIN animals ON animals.id = visits.animals_id
+    WHERE vets.name = 'Maisy Smith'
+    GROUP BY animals.name, visits.date_of_visit
+    ORDER BY first_visit LIMIT 1;
 
 -- SELECT * FROM visits
 --     INNER JOIN vets ON vets.id = visits.vets_id
